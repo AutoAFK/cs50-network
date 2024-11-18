e69b65847aa329eef082940432a220f4a5d8576c
AutoAFK <ranhaim85@gmail.com>
2024-11-18 19:23:24 +0200

    Fixed user page won't load the right page or not load at all

--- users/views.py
+++ users/views.py
@@ -5,14 +5,14 @@ from .models import User
 # Create your views here.
 def user_page(request, id):
     user = User.objects.get(id=id)
-    return render(request, "users/user_page.html", {user: user})
+    return render(request, "users/user_page.html", {"user": user})
 
 
 def user_followers(request, id):
     user = User.objects.get(id=id)
     followers = user.followers.all()
     return render(
-        request, "users/user_followers.html", {user: user, followers: followers}
+        request, "users/user_followers.html", {"user": user, "followers": followers}
     )
 
 
@@ -20,5 +20,5 @@ def user_following(request, id):
     user = User.objects.get(id=id)
     following = user.following.all()
     return render(
-        request, "users/user_following.html", {user: user, following: following}
+        request, "users/user_following.html", {"user": user, "following": following}
     )
